eeb269264ed6cc9bf0e5823214026474023fe6ed
sujeebadal <sujeebadal@gmail.com>
2019-02-13 17:49:19 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome, let's play hangman!");
+
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("tree");
+        words.add("rain");
+        words.add("bear");
+        words.add("encourage");
+        words.add("promise");
+        words.add("soup");
+        words.add("chess");
+        words.add("insurance");
+        words.add("pancake");
+        words.add("stream");
+        Random rand = new Random();
+        //        System test
+         //   int randomIndex = (int) (Math.random() * words.size());
+            //System.out.println(words.get(randomIndex));
+
+
+    }
+
+        }
+
+
